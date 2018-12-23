dc5b27e8b6e30abcbeba024f182c7d12e5e35fed
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-23 18:00:55 -0500

    My operators commit

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+if speed > 60 || speed < 40
+	return true
+else return false
 end
-
-
-
+end
+unsafe?(70)
 def not_safe?(speed)
-	
+speed > 60 || speed < 40 ? return true : return false
 end
-	
-
-
+not_safe(50)
--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,4 +1,5 @@
-
+require_relative './spec_helper.rb'
+require './operations.rb'
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
 
@@ -41,5 +42,5 @@ describe "Operations" do
       expect(match).to include("?")
     end
   end
-  
+
 end
